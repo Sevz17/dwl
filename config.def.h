2c1c914d1fcde3d851a5c7078cb98c36ef489a7c
Sevz17 <leohdz172@outlook.com>
2021-04-02 11:46:28 -0600

    Disabled pointer accelerantion and changed config

--- config.def.h
+++ config.def.h
@@ -1,12 +1,12 @@
 /* appearance */
 static const int sloppyfocus        = 1;  /* focus follows mouse */
-static const unsigned int borderpx  = 1;  /* border pixel of windows */
+static const unsigned int borderpx  = 2;  /* border pixel of windows */
 static const float rootcolor[]      = {0.3, 0.3, 0.3, 1.0};
-static const float bordercolor[]    = {0.5, 0.5, 0.5, 1.0};
-static const float focuscolor[]     = {1.0, 0.0, 0.0, 1.0};
+static const float focuscolor[]    = {0.4, 0.0, 1.0, 1.0};
+static const float bordercolor[]     = {0.0, 0.0, 0.0, 0.7};
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { " ", "﬏ ", " ", " ", " ", " ", " ", " ", " ", " " };
 
 static const Rule rules[] = {
 	/* app_id     title       tags mask     isfloating   monitor */
@@ -22,6 +22,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ NULL,       NULL },
 };
 
 /* monitors
@@ -33,7 +34,7 @@ static const MonitorRule monrules[] = {
 	{ "eDP-1",    0.5,  1,      2,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL, 0, 0 },
 	*/
 	/* defaults */
-	{ NULL,       0.55, 1,      1,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL, 0, 0 },
+	{ NULL,       0.50, 1,      1,    &layouts[2], WL_OUTPUT_TRANSFORM_NORMAL, 0, 0 },
 };
 
 /* keyboard */
@@ -42,6 +43,10 @@ static const struct xkb_rule_names xkb_rules = {
 	/* example:
 	.options = "ctrl:nocaps",
 	*/
+	.layout  = "latam,latam,us",
+	.model   = "pc105",
+	.variant = "dvorak,,",
+	.options = "grp:win_space_toggle , keypad:pointerkeys , terminate:ctrl_alt_bksp",
 };
 
 static const int repeat_rate = 25;
@@ -50,11 +55,18 @@ static const int repeat_delay = 600;
 /* Time (in miliseconds) until the cursor will hidde */
 static const int cursor_inactive = 1000;
 
+/* Disable acceleration */
+static const int disable_mouse_acceleration = 1;
+
 /* Trackpad */
 static const int tap_to_click = 1;
 static const int natural_scrolling = 0;
 
-#define MODKEY WLR_MODIFIER_ALT
+#define MODKEY WLR_MODIFIER_LOGO
+#define ALTKEY WLR_MODIFIER_ALT
+#define Control WLR_MODIFIER_CTRL
+#define Shift WLR_MODIFIER_SHIFT
+
 #define TAGKEYS(KEY,SKEY,TAG) \
 	{ MODKEY,                    KEY,            view,            {.ui = 1 << TAG} }, \
 	{ MODKEY|WLR_MODIFIER_CTRL,  KEY,            toggleview,      {.ui = 1 << TAG} }, \
@@ -71,39 +83,93 @@ static const char *menucmd[] = { "bemenu-run", NULL };
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
 	/* modifier                  key                 function        argument */
-	{ MODKEY,                    XKB_KEY_p,          spawn,          {.v = menucmd} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Return,     spawn,          {.v = termcmd} },
-	{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
-	{ MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
-	{ MODKEY,                    XKB_KEY_i,          incnmaster,     {.i = +1} },
-	{ MODKEY,                    XKB_KEY_d,          incnmaster,     {.i = -1} },
-	{ MODKEY,                    XKB_KEY_h,          setmfact,       {.f = -0.05} },
-	{ MODKEY,                    XKB_KEY_l,          setmfact,       {.f = +0.05} },
-	{ MODKEY,                    XKB_KEY_Return,     zoom,           {0} },
-	{ MODKEY,                    XKB_KEY_Tab,        view,           {0} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_C,          killclient,     {0} },
-	{ MODKEY,                    XKB_KEY_t,          setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                    XKB_KEY_f,          setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                    XKB_KEY_m,          setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                    XKB_KEY_space,      setlayout,      {0} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_space,      togglefloating, {0} },
-	{ MODKEY, 					 XKB_KEY_e,    		togglefullscreen, {0} },
-	{ MODKEY,                    XKB_KEY_0,          view,           {.ui = ~0} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_parenright, tag,            {.ui = ~0} },
-	{ MODKEY,                    XKB_KEY_comma,      focusmon,       {.i = WLR_DIRECTION_LEFT} },
-	{ MODKEY,                    XKB_KEY_period,     focusmon,       {.i = WLR_DIRECTION_RIGHT} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_less,       tagmon,         {.i = WLR_DIRECTION_LEFT} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_greater,    tagmon,         {.i = WLR_DIRECTION_RIGHT} },
+
+    /* ------------------ Windows ------------------ */
+
+	/* Switch between windows */
+	{ MODKEY,                    XKB_KEY_r,          focusstack,     {.i = +1} },
+	{ MODKEY,                    XKB_KEY_t,          focusstack,     {.i = -1} },
+
+	/* Change window sizes */
+	{ MODKEY,                    XKB_KEY_c,          incnmaster,     {.i = +1} },
+	{ MODKEY|Shift,              XKB_KEY_C,          incnmaster,     {.i = -1} },
+	{ MODKEY,                    XKB_KEY_d,          setmfact,       {.f = -0.05} },
+	{ MODKEY,                    XKB_KEY_n,          setmfact,       {.f = +0.05} },
+
+	/* Toggle floating */
+	{ MODKEY|Control,            XKB_KEY_u,          togglefloating, {0} },
+
+	/* Toggle fullscreen */
+	{ MODKEY|Shift,              XKB_KEY_U,          togglefullscreen, {0} },
+
+	/* Cycle layouts */
+	{ MODKEY,                    XKB_KEY_Tab,        cyclelayout,    {.i = +1 } },
+	{ MODKEY|Shift,              XKB_KEY_Tab,        cyclelayout,    {.i = -1 } },
+
+	/* Focus next - prev monitor */
+	{ MODKEY,                    XKB_KEY_w,          focusmon,       {.i = WLR_DIRECTION_LEFT} },
+	{ MODKEY,                    XKB_KEY_v,          focusmon,       {.i = WLR_DIRECTION_RIGHT} },
+
+	/* Send window to next - prev monitor */
+	{ MODKEY|Shift,              XKB_KEY_W,          tagmon,         {.i = WLR_DIRECTION_LEFT} },
+	{ MODKEY|Shift,              XKB_KEY_V,          tagmon,         {.i = WLR_DIRECTION_RIGHT} },
+
+	/* Kill window */
+	{ MODKEY,                    XKB_KEY_comma,      killclient,     {0} },
+
+	/* Quit dwl */
+	{ MODKEY|Control,            XKB_KEY_period,     quit,           {0} },
+
+//	/* Restart dwl */
+//	{ MODKEY|Control,            XKB_KEY_p,          quit,           {1} },
+
+    /* Shutdown computer */
+    { ALTKEY|Control,            XKB_KEY_Delete,     spawn,          SHCMD("shutdown now") },
+
+    /* Restart computer */
+    { ALTKEY|Control,            XKB_KEY_Insert,     spawn,          SHCMD("reboot") },
+
+    /* Hibernate computer */
+    { MODKEY|Control,            XKB_KEY_Delete,     spawn,          SHCMD("systemctl hibernate") },
+
+    /* Suspend computer */
+    { MODKEY|Control,            XKB_KEY_Insert,     spawn,          SHCMD("systemctl suspend") },
+
+	
+    /* ---------------- Workspaces ----------------- */
 	TAGKEYS(          XKB_KEY_1, XKB_KEY_exclam,                     0),
-	TAGKEYS(          XKB_KEY_2, XKB_KEY_at,                         1),
+	TAGKEYS(          XKB_KEY_2, XKB_KEY_quotedbl,                   1),
 	TAGKEYS(          XKB_KEY_3, XKB_KEY_numbersign,                 2),
 	TAGKEYS(          XKB_KEY_4, XKB_KEY_dollar,                     3),
 	TAGKEYS(          XKB_KEY_5, XKB_KEY_percent,                    4),
-	TAGKEYS(          XKB_KEY_6, XKB_KEY_caret,                      5),
-	TAGKEYS(          XKB_KEY_7, XKB_KEY_ampersand,                  6),
-	TAGKEYS(          XKB_KEY_8, XKB_KEY_asterisk,                   7),
-	TAGKEYS(          XKB_KEY_9, XKB_KEY_parenleft,                  8),
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Q,          quit,           {0} },
+	TAGKEYS(          XKB_KEY_6, XKB_KEY_ampersand,                  5),
+	TAGKEYS(          XKB_KEY_7, XKB_KEY_slash,                      6),
+	TAGKEYS(          XKB_KEY_8, XKB_KEY_parenleft,                  7),
+	TAGKEYS(          XKB_KEY_9, XKB_KEY_parenright,                 8),
+	TAGKEYS(          XKB_KEY_0, XKB_KEY_equal,                      9),
+
+	/* Switch between the last tag and the current */
+	{ MODKEY,                    XKB_KEY_z,          view,           {0} },
+
+    /* ------------------- Apps -------------------- */
+
+	{ MODKEY,                    XKB_KEY_Menu,       spawn,          {.v = menucmd} },
+
+	/* wofi */
+	{ MODKEY,                    XKB_KEY_m,          spawn,          SHCMD("wofi --show=drun") },
+
+	/* Terminal emulators */
+	{ MODKEY,                    XKB_KEY_Return,     spawn,          {.v = termcmd} },
+//	{ MODKEY,                    XKB_KEY_Return,     zoom,           {0} },
+//	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_parenright, tag,            {.ui = ~0} },
+
+    /* ----------------- Hardware ------------------ */
+
+    /* Volume */
+    {MODKEY,                     XKB_KEY_apostrophe, spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -2%")},
+    {MODKEY,                     XKB_KEY_questiondown,spawn,         SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +2%")},
+    {MODKEY,                     XKB_KEY_BackSpace,  spawn,          SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle")},
+
 
 	/* Ctrl-Alt-Backspace and Ctrl-Alt-Fx used to be handled by X server */
 	{ WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_Terminate_Server, quit, {0} },
