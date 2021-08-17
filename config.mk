d51e13ff2026b402351e21da4db4e91eb983dcc4
Sevz17 <leohdz172@outlook.com>
2021-08-16 19:48:01 -0500

    Revert "disable xwayland"
    This reverts commit ad9f5145ed27974083ddee2e7b55c1b75267b028.

--- config.mk
+++ config.mk
@@ -8,7 +8,7 @@ PREFIX = /usr/local
 CFLAGS ?= -g -Wall -Wextra -Werror -Wno-unused-parameter -Wno-sign-compare -Wno-unused-function -Wno-unused-variable -Wdeclaration-after-statement -Wno-unused-result
 
 # Uncomment to build XWayland support
-#CFLAGS += -DXWAYLAND
+CFLAGS += -DXWAYLAND
 
 # Optimizations
 CFLAGS += -march=native
