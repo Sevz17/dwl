fcf3f602aff38522c247857f01e2bb8bdbfefac1
Leonardo Hernández Hernández <leohdz172@protonmail.com>
2022-07-20 00:15:32 -0500

    allow gaps in monocle layout if requested

--- dwl.c
+++ dwl.c
@@ -2036,8 +2036,12 @@ monocle(Monitor *m)
 	wl_list_for_each(c, &clients, link) {
 		if (!VISIBLEON(c, m) || c->isfloating || c->isfullscreen)
 			continue;
-		resize(c, m->w, 0, !smartborders);
 		n++;
+		if (!monoclegaps)
+			resize(c, m->w, 0, !smartborders);
+		else
+			resize(c, (struct wlr_box){.x = m->w.x + gappoh, .y = m->w.y + gappov,
+				.width = m->w.width - 2 * gappoh, .height = m->w.height - 2 * gappov}, 0, !smartborders);
 	}
 	if (n)
 		snprintf(m->ltsymbol, LENGTH(m->ltsymbol), "[%d]", n);
